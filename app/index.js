c4b9841f3556a3787249756ab09ba0d87b552981
DhruvKumar2005 <156638526+DhruvKumar2005@users.noreply.github.com>
2025-07-29 20:41:12 +0530

    my last deployment

---
+++ app/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path=require("path");
+const port = 4004;
+const ejs = require("ejs");
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.set("views",path.join(__dirname,"views"));
+// 
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
+
+app.get("/", (req,res)=>{
+    res.render("home");
+})
+
+
+modules.exports=app;
